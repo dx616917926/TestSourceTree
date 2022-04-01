071e9db2121ba88dd1b655c8f1b69433a8f50975
dx <616917926@qq.com>
2022-04-01 23:29:38 +0800

    no message

--- TestSourceTree/AppDelegate.m
+++ TestSourceTree/AppDelegate.m
@@ -37,6 +37,9 @@
 
 
 - (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
+    
+    
+    ///78888888
     // Called when the user discards a scene session.
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
