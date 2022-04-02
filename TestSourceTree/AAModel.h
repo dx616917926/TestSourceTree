b02a98090acc2e8b066fc37f6a0c04b1dadda929
dx <616917926@qq.com>
2022-04-02 16:06:52 +0800

    no message

---
+++ TestSourceTree/AAModel.h
@@ -0,0 +1,17 @@
+//
+//  AAModel.h
+//  TestSourceTree
+//
+//  Created by 邓雄 on 2022/4/2.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AAModel : NSObject
+
+@property(nonatomic,strong) NSString *hhhh;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestSourceTree/AAModel.m
@@ -0,0 +1,12 @@
+//
+//  AAModel.m
+//  TestSourceTree
+//
+//  Created by 邓雄 on 2022/4/2.
+//
+
+#import "AAModel.h"
+
+@implementation AAModel
+
+@end
